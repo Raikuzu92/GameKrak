36765d95756e6e10ac26eccb24eedce663403e2e
justmacn <172618812+justmacn@users.noreply.github.com>
2024-09-05 14:59:57 -0400

    added transactions to User model

--- server/models/User.js
+++ server/models/User.js
@@ -25,6 +25,12 @@ const userSchema = new Schema({
       ref: 'Listing'
     }
   ],
+  transactions: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Transaction'
+    }
+  ],
 });
 
 userSchema.pre("save", async function (next) {
