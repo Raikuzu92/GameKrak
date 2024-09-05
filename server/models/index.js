1f24c6031c06f1a68fc876a5ee213672f44427dd
Raikuzu <robman455@gmail.com>
2024-09-05 14:10:27 -0400

    index model fixed

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,7 @@
 const User = require("./User");
 const Game = require("./Game");
+const Listing = require("./Listing");
+const Transaction = require("./Transactions")
 
-module.exports = { User, Game };
+
+module.exports = { User, Game, Listing, Transaction };
