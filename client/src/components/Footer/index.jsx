e5e367f6b9b6cabfa026a55d02562b86f96bba89
justmacn <172618812+justmacn@users.noreply.github.com>
2024-09-09 15:53:21 -0400

    styled header, footer not finished

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -3,8 +3,8 @@ import './Footer.css'; // Custom styling for the footer
 const Footer = () => {
   return (
     <footer className="retro-footer">
-      <div className="container text-center py-3">
-        <p className="mb-0 text-white">
+      <div className="container d-flex text-center py-3 align-items-center">
+        <p className="text-white p-override">
           © 2024 GameKrak. All Rights Reserved.
         </p>
       </div>
