7f9b5d7ecbac73ebd180e83c3d93498fce727432
Raikuzu <robman455@gmail.com>
2024-09-11 11:45:38 -0400

    fixed the coin finally

---
+++ client/src/components/Cartlogo/Cartlogo.jsx
@@ -0,0 +1,13 @@
+// src/components/CartLogo.js
+import React from 'react';
+import './CartLogo.css'; // Import CSS for styling
+
+const CartLogo = () => {
+  return (
+    <div className="cart-logo">
+      <i className="fas fa-shopping-cart"></i> {/* Font Awesome icon */}
+    </div>
+  );
+};
+
+export default CartLogo;
--- client/src/components/CoinSlot/CoinSlot.jsx
+++ client/src/components/CoinSlot/CoinSlot.jsx
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
 import './CoinSlot.css'; 
-import Coin from '../Coin/Coin';
 
 const CoinSlot = () => {
-    const [isCoinMoving, setIsCoinMoving] = useState(false);
 
-    const handleStartClick = () => {
-        setIsCoinMoving(true);
-    };
 
     return (
         <div>
-            <button onClick={handleStartClick}>Start</button> {/* Button to trigger coin animation */}
+            {/* Button to trigger coin animation */}
             <div className="coin-slot">
                 <div className="slot-gap"></div> {/* Represents the visible part of the slot */}
-                <Coin start={isCoinMoving} /> {/* Represents the coin */}
+         
             </div>
         </div>
     );
