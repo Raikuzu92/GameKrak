cce141e7ece98241f1a7060baeb20a39cb9f5a53
Eric Hernandez <derickhh.05@gmail.com>
2024-09-12 12:35:51 -0400

    added styling for buy sell trade page

--- client/src/components/Sell/Sell.jsx
+++ client/src/components/Sell/Sell.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_SELL_LISTINGS } from '../../utils/queries'; // Import the new query
+import { QUERY_SELL_LISTINGS } from '../../utils/queries';
+import './Sell.css'; // Custom CSS for Sell page
 
-const Buy = () => {
-  const { loading, data, error } = useQuery(QUERY_SELL_LISTINGS); // Use the QUERY_BUY_LISTINGS
+const Sell = () => {
+  const { loading, data, error } = useQuery(QUERY_SELL_LISTINGS);
   const listings = data?.listings || [];
 
-  // Handle loading state
   if (loading) {
     return <div>Loading listings...</div>;
   }
 
-  // Handle error state
   if (error) {
     return <div>Error loading listings. Please try again later.</div>;
   }
 
-  // Handle empty data
   if (!listings.length) {
-    return <div>No games available for purchase at the moment.</div>;
+    return <div>No games available for sale at the moment.</div>;
   }
 
   return (
-    <div>
-      <h2>Sell Games</h2>
-      <div>
+    <div id='sell-page'>
+      <h2 id="sell-heading">Sell Games</h2>
+      <div id="sell-listing-cards">
         {listings.map((listing) => (
-          <div key={listing._id} className='card mb-3'>
-            <h4 className='card-header bg-dark text-light p-2 m-0'>{listing.game.title}</h4>
-            <div className='card-body bg-light p-2'>
-              <h5>Price: ${listing.price}</h5>
+          <div key={listing._id} className='sell-card'>
+            <div className='sell-left'>
+              <img 
+                src={`https://www.vgchartz.com${listing.game.img}`}
+                alt={listing.game?.title} 
+                className='sell-game-img'
+              />
+            </div>
+            <div className='sell-right'>
+              <h2 className='sell-card-header'><strong>{listing.game?.title}</strong></h2>
+              <h5 className="pt-3">Price: ${listing.price}</h5>
               <p>{listing.description}</p>
               <p>Condition: {listing.condition}</p>
               <p>Seller: {listing.user.username}</p>
@@ -41,4 +46,4 @@ const Buy = () => {
   );
 };
 
-export default Buy;
+export default Sell;
