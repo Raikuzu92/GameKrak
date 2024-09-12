02c3103e99eecb5145575bfe5100ae085d57e004
Raikuzu <robman455@gmail.com>
2024-09-12 15:34:23 -0400

    changed import index

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,7 +1,7 @@
 const db = require("../config/connection");
 const { User, Game } = require("../models");
 const userSeeds = require("./userSeeds.json");
-const gameSeeds = require("./gameSeeds.json");
+const gameSeeds = require("./gameData.json");
 const cleanDB = require("./cleanDB");
 
 db.once("open", async () => {
